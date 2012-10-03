e9fa96af99046a045089152df3370cdfb26e39cf
Lucimar Leandro Bezerra <lucimarleandro@gmail.com>
2012-10-03 10:27:03 -0300

    Criação do modelo District

---
+++ app/Model/District.php
@@ -0,0 +1,57 @@
+<?php
+App::uses('AppModel', 'Model');
+/**
+ * District Model
+ *
+ * @property City $City
+ * @property Address $Address
+ */
+class District extends AppModel {
+
+/**
+ * Display field
+ *
+ * @var string
+ */
+	public $displayField = 'name';
+
+
+	//The Associations below have been created with all possible keys, those that are not needed can be removed
+
+/**
+ * belongsTo associations
+ *
+ * @var array
+ */
+	public $belongsTo = array(
+		'City' => array(
+			'className' => 'City',
+			'foreignKey' => 'city_id',
+			'conditions' => '',
+			'fields' => '',
+			'order' => ''
+		)
+	);
+
+/**
+ * hasMany associations
+ *
+ * @var array
+ */
+	public $hasMany = array(
+		'Address' => array(
+			'className' => 'Address',
+			'foreignKey' => 'district_id',
+			'dependent' => false,
+			'conditions' => '',
+			'fields' => '',
+			'order' => '',
+			'limit' => '',
+			'offset' => '',
+			'exclusive' => '',
+			'finderQuery' => '',
+			'counterQuery' => ''
+		)
+	);
+
+}
